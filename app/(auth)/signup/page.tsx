afeaaf17dd1120ee86d6b29c8bbb5a63947e09db
quantumCode <quantumcode22@gmail.com>
2026-01-04 20:44:14 +0530

    feat: Set up initial application structure, authentication flows, and foundational UI components.

---
+++ app/(auth)/signup/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { RegisterForm } from "@/features/auth/components/register-form";
+
+const Page = () => {
+  return (
+    <div>
+      <RegisterForm />
+    </div>
+  );
+};
+
+export default Page;
